a7d1bf1d515da829b18fb84837c959ea0dc2e4ea
jari26 <107353692+jari26@users.noreply.github.com>
2023-09-10 22:05:51 +0900

    Update js_preview.js

--- js_preview.js
+++ js_preview.js
@@ -1,16 +1,14 @@
-// JavaScript Document
-window.onload = function(){
 const $text = document.querySelector(".typing .text");
 
 // 글자 모음
 const letters = [
-  "COLLECTIVE",
-  "CREATORS", 
-  "TROUBLEMAKERS"
+  "CREATORS",
+  "TROUBLEMAKERS", 
+  "COLLEVTIVE"
 ];
 
 // 글자 입력 속도
-const speed = 80;
+const speed = 50;
 let i = 0;
 
 // 타이핑 효과
@@ -23,7 +21,7 @@ const typing = async () => {
   }
   
   // 잠시 대기
-  await wait(100);
+  await wait(300);
   
   // 지우는 효과
   remove();
@@ -42,18 +40,17 @@ const remove = async () => {
   
   // 다음 순서의 글자로 지정, 타이핑 함수 다시 실행
   i = !letters[i+1] ? 0 : i + 1;
-  typing();
+	typing();
+	i==2;
+	setTimeout(function(){ window.location.replace('main.html')}, 2800);
+
 }
 
 // 딜레이 기능 ( 마이크로초 )
+
 function wait(ms) {
   return new Promise(res => setTimeout(res, ms))
 }
 
-// 초기 실행
-var myFunc = function() {
-  window.location.href = './main.html';
-    });
-
-setTimeout(myFunc, 1500);
-}
+// 페이지전환
+setTimeout(typing, 500);
